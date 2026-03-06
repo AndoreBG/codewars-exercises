ad5f4997e03744e29d122908d21b8db166749c38
André Gorzoni <andreboolandim2@gmail.com>
2026-03-06 11:39:24 -0300

    Set Alarm

---
+++ code-wars/SetAlarm/Program.cs
@@ -0,0 +1,9 @@
+﻿static bool SetAlarm(bool employed, bool vacation) => employed && !vacation;
+
+Console.WriteLine(SetAlarm(true, false));
+
+Console.WriteLine(SetAlarm(false, false));
+
+Console.WriteLine(SetAlarm(true, true));
+
+Console.WriteLine(SetAlarm(false, true));
