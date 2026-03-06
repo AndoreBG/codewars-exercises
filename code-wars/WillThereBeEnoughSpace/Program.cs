5442ff815ab5b6563fa09fac720f39d554c1ab43
André Gorzoni <andreboolandim2@gmail.com>
2026-03-06 10:47:42 -0300

    Will there be enough space?

---
+++ code-wars/WillThereBeEnoughSpace/Program.cs
@@ -0,0 +1,9 @@
+﻿static int Enough(int cap, int on, int wait) => on + wait <= cap ? 0 : (on + wait) - cap;
+
+Console.WriteLine(Enough(10, 5, 5));
+
+Console.WriteLine(Enough(10, 6, 5));
+
+Console.WriteLine(Enough(10, 5, 6));
+
+Console.WriteLine(Enough(10, 60, 50));
