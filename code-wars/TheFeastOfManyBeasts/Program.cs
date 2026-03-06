04c2704d5c4495b5e6caefae9996303b07725113
André Gorzoni <andreboolandim2@gmail.com>
2026-03-06 11:04:11 -0300

    The Feast Of Many Beasts?

---
+++ code-wars/TheFeastOfManyBeasts/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.Contracts;
+
+static bool Feast(string beast, string dish) => beast.First() == dish.First() && beast.Last() == dish.Last() ;
+
+Console.WriteLine(Feast("cavalo", "chocolate"));
+
+Console.WriteLine(Feast("pescada", "paçoca"));
+
+Console.WriteLine(Feast("brown bear", "bear claw"));
+
