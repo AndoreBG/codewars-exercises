92fa7585cfe005a9030fc707096cc49dfda3522d
André Gorzoni <andreboolandim2@gmail.com>
2026-03-09 12:26:44 -0300

    Multiples Of 3 or 5

---
+++ code-wars/MultiplesOf3Or5/Program.cs
@@ -0,0 +1,23 @@
+﻿static int Solution(int value)
+{
+    if (value < 0) return 0;
+
+    int r = 0;
+
+    for (int i = 0; i < value; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            r += i;
+        }
+    }
+
+    return r;
+}
+
+static int Solution2(int value) => value < 0 ? 0 : Enumerable.Range(0, value).Where(i => i % 3 == 0 || i % 5 == 0).Sum();
+
+Console.WriteLine(Solution(10));
+Console.WriteLine(Solution(-1));
+Console.WriteLine(Solution2(10));
+Console.WriteLine(Solution2(-1));
