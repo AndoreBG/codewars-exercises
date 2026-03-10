3f3739fe21742ace756e7b20f73737f66a791fca
André Gorzoni <andreboolandim2@gmail.com>
2026-03-10 14:32:45 -0300

    Reversed Sequence

---
+++ code-wars/ReversedSequence/Program.cs
@@ -0,0 +1,7 @@
+﻿static int[] ReverseSeq(int n) => Enumerable.Range(1, n).Reverse().ToArray();
+
+Console.WriteLine(ReverseSeq(5)[0]);
+Console.WriteLine(ReverseSeq(5)[1]);
+Console.WriteLine(ReverseSeq(5)[2]);
+Console.WriteLine(ReverseSeq(5)[3]);
+Console.WriteLine(ReverseSeq(5)[4]);
